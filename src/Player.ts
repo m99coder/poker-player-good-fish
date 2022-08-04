e0b6f572ff90790f724163e9ccf570128c383c55
Marco Lehmann <m99@posteo.de>
2022-08-04 10:50:37 +0200

    all-in always

--- src/Player.ts
+++ src/Player.ts
@@ -4,12 +4,12 @@ export class Player {
 
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     // this will fold all the time
-    betCallback(0)
+    // betCallback(0)
 
     // phase 1 –––
     // directly go all-in until a better strategy is implemented
     // assuming it’s a sit’n’go and every round is new, this is valid
-    // betCallback(gameState.players[gameState.in_action].stack)
+    betCallback(gameState.players[gameState.in_action].stack)
   }
 
   public showdown(_gameState: GameState): void {
