675e1ab4f55a875339cbe4a26d2c5a32ab50ecf9
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2022-08-04 13:26:43 +0200

    more logs

--- src/actions.ts
+++ src/actions.ts
@@ -7,16 +7,20 @@ import { GameState } from "./GameState";
 
 export function check(gameState: GameState, betCallback: (bet: number) => void): void {
     if (gameState.current_buy_in === gameState.players[gameState.in_action].bet) {
+        console.log('ACTION: check function: check with 0')
         betCallback(0) // no more raise
     } else {
+        console.log(`ACTION: checking with current_buy: ${gameState.current_buy_in}`)
         betCallback(gameState.current_buy_in) // we raise
     }
 }
 
 export function fold(gameState: GameState, betCallback: (bet: number) => void): void {
+    console.log(`ACTION: FOLD!`)
     betCallback(0)
 }
 
 export function allIn(gameState: GameState, betCallback: (bet: number) => void): void {
+    console.log(`ACTION: ALL IN!!!!`)
     betCallback(gameState.players[gameState.in_action].stack)
 }
