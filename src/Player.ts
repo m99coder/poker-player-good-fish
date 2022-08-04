3ec78b5807de80318f16bb2b31fdbd1f9f653e42
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2022-08-04 14:53:53 +0200

    new logs

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,7 @@
 import { GameState, Card as GameCard } from "./GameState";
 import { Card, Evaluator } from "deuces.js";
 import { allIn, callOrCheck, fold, raise } from "./actions";
+import { logGameStatus } from "./helpers";
 
 export class Player {
   private encodeCard(card: GameCard): string {
@@ -12,8 +13,6 @@ export class Player {
     gameState: GameState,
     betCallback: (bet: number) => void
   ): void {
-    console.log("betRequest", gameState);
-
     // encode hole cards
     const hand = gameState.players[gameState.in_action].hole_cards.map((c) =>
       Card.newCard(this.encodeCard(c))
@@ -25,9 +24,10 @@ export class Player {
     );
 
     const evaluator = new Evaluator();
+    let rank = evaluator.evaluate(board, hand);
+    let percentage = 1.0 - evaluator.get_five_card_rank_percentage(rank);
 
-    console.log("Hole cards");
-    Card.print_pretty_cards(hand, true);
+
 
     // this will fold all the time
     // betCallback(0)
@@ -35,6 +35,7 @@ export class Player {
     // phase 1 –––
     // directly go all-in until a better strategy is implemented
     // assuming it’s a sit’n’go and every round is new, this is valid
+    logGameStatus(gameState, hand, board, percentage, rank, evaluator);
     if (gameState.community_cards.length === 0) {
       callOrCheck(gameState, betCallback);
     } else {
@@ -42,17 +43,6 @@ export class Player {
       // Hand strength is valued on a scale of 1 to 7462,
       // where 1 is a Royal Flush and 7462 is unsuited 7-5-4-3-2,
       // as there are only 7642 distinctly ranked hands in poker.
-
-      let rank = evaluator.evaluate(board, hand);
-      let rankClass = evaluator.get_rank_class(rank);
-      let percentage = 1.0 - evaluator.get_five_card_rank_percentage(rank);
-      Card.print_pretty_cards(board, true);
-      console.log(
-        `Rank for your hand is: ${rank} (${evaluator.class_to_string(
-          rankClass
-        )})`
-      );
-      console.log(percentage);
 
       if (percentage > 0.9) {
         allIn(gameState, betCallback);
