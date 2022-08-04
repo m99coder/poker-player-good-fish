0d5719c0fb9617c92673b49be12e6153b86f03ca
Marco Lehmann <m99@posteo.de>
2022-08-04 10:37:16 +0200

    update boilerplate

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,20 @@
+import { GameState } from './GameState'
+
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+
+  public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
+    // this will fold all the time
+    betCallback(0)
+
+    // phase 1 –––
+    // directly go all-in until a better strategy is implemented
+    // assuming it’s a sit’n’go and every round is new, this is valid
+    // betCallback(gameState.players[gameState.in_action].stack)
   }
 
-  public showdown(gameState: any): void {
-
+  public showdown(_gameState: GameState): void {
+    // for implementing learning algorithms only
   }
-};
+}
 
-export default Player;
+export default Player
